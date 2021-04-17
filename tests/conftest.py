891699090dd0fc0fa3d0e660c05eec8e32e1dd8d
Takos22 <takos2210@gmail.com>
2021-04-17 23:51:25 +0200

    Add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,4 +6,9 @@ import baguette
 @pytest.fixture(name="app")
 async def app_test_client():
     app = baguette.Baguette()
+
+    @app.route("/")
+    async def index(request):
+        return "Hello, world!"
+
     return baguette.testing.TestClient(app)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,8 @@
 import pytest
+from baguette.testing import TestClient
 
 
 @pytest.mark.asyncio
-async def test_app(app):
-    assert True  # TODO: real testing
+async def test_app(app: TestClient):
+    response = await app.request("GET", "/")
+    assert response.body == "Hello, world!".encode("utf-8")
