08f438e98220a5ed364c3004fcd0da1fc5fa3fe6
Takos22 <takos2210@gmail.com>
2021-05-15 17:31:30 +0200

    Update linting and testing scripts

---
+++ scripts/lint-code.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -e
+set -x
+
+folders="baguette tests examples"
+
+# stop the build if there are Python syntax errors or undefined names
+flake8 $folders --count --select=E9,F63,F7,F82 --show-source --statistics
+# exit-zero treats all errors as warnings
+flake8 $folders --count --exit-zero --statistics
+
+# check formatting with black
+black $folders --check --line-length 80
+
+# check import ordering with isort
+isort $folders --check-only
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 set -e
-set -x
 
-# lint first
-bash ./scripts/lint.sh
+lint=true
+
+# no linitng if -n or --no-lint flag
+for arg in "$@"
+do
+    if [ "$arg" == "-n" ] || [ "$arg" == "--no-lint" ]; then
+        lint=false
+    fi
+done
+
+if [ "$lint" = true ]; then
+    set -x
+    # lint
+    ./scripts/lint.sh
+else
+    set -x
+fi
 
 # run tests
 pytest
