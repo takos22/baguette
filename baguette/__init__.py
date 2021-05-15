7ae703a7daa1c2d73ebb8877100778ae69639c30
Takos22 <takos2210@gmail.com>
2021-05-15 22:57:13 +0200

    Bump version to 0.2.1

--- baguette/__init__.py
+++ baguette/__init__.py
@@ -11,11 +11,11 @@ VersionInfo = NamedTuple(
     "VersionInfo", major=int, minor=int, micro=int, releaselevel=str, serial=int
 )
 
-version_info = VersionInfo(major=0, minor=2, micro=0, releaselevel="", serial=0)
+version_info = VersionInfo(major=0, minor=2, micro=1, releaselevel="", serial=0)
 
 __title__ = "baguette"
 __author__ = "takos22"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 __all__ = [
     "Baguette",
