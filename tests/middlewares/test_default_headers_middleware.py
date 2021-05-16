7c517247706512005fd42b92c911215e6e82ea1c
Takos22 <takos2210@gmail.com>
2021-05-16 13:16:48 +0200

    Add middleware tests

---
+++ tests/middlewares/test_default_headers_middleware.py
@@ -0,0 +1,34 @@
+import pytest
+
+from baguette.app import Baguette
+from baguette.responses import Response
+
+from ..conftest import create_test_request
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    [
+        "headers",
+        "expected_headers",
+    ],
+    [
+        [{}, {"server": "baguette"}],
+        [
+            {"content-type": "text/plain"},
+            {"content-type": "text/plain", "server": "baguette"},
+        ],
+    ],
+)
+async def test_error_middleware(headers, expected_headers):
+    app = Baguette(default_headers={"server": "baguette"})
+
+    @app.route("/")
+    async def index(request):
+        return Response(body="", headers=request.headers)
+
+    request = create_test_request(headers=headers)
+    response = await app.handle_request(request)
+    assert len(response.headers) == len(expected_headers)
+    for name, value in expected_headers.items():
+        assert response.headers[name] == value
