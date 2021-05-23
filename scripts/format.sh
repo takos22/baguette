44343e476a5cb1070d47a567b6e6cb6a3cb66d18
Takos22 <takos2210@gmail.com>
2021-05-23 16:56:02 +0200

    Update format script

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 set -e
+
+folders="baguette tests examples"
+
 set -x
 
 # put every import on one line for autoflake remove unused imports
-isort baguette tests examples --force-single-line-imports
+isort $folders --force-single-line-imports
 # remove unused imports and variables
-autoflake baguette tests examples --remove-all-unused-imports --recursive --remove-unused-variables --in-place --exclude=__init__.py
+autoflake $folders --remove-all-unused-imports --recursive --remove-unused-variables --in-place --exclude=__init__.py
 
 # format code
-black baguette tests examples --line-length 80
+black $folders --line-length 80
 
 # resort imports
-isort baguette tests examples
+isort $folders
