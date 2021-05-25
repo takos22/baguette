3defbd97112c19521de24acb38d15ef82611e1a4
Takos22 <takos2210@gmail.com>
2021-05-25 23:27:44 +0200

    Add websocket example

---
+++ examples/websocket.py
@@ -0,0 +1,54 @@
+import logging
+
+from baguette import Baguette, Websocket
+from baguette.utils import address_to_str
+
+app = Baguette()
+
+
+logger = logging.getLogger("uvicorn.websocket")
+
+connections = 0
+MAX_CONCURRENT_CONNECTIONS = 5
+
+
+@app.websocket("/")
+class IndexWebsocket(Websocket):
+    connections = 0
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    async def on_connect(self):
+        global connections
+        if self.connections >= MAX_CONCURRENT_CONNECTIONS:
+            raise RuntimeError("Too many connections")
+
+        self.connections += 1
+        logger.info(
+            "{} connected (connections: {})".format(
+                address_to_str(self.client), self.connections
+            )
+        )
+
+    async def on_message(self, message: str):
+        logger.info("{} sent: {}".format(address_to_str(self.client), message))
+
+        send_message = message.upper()
+        await self.send(send_message)
+        logger.info(
+            "{} was sent: {}".format(address_to_str(self.client), send_message)
+        )
+
+    async def on_disconnect(self, code: int):
+        global connections
+        self.connections -= 1
+        logger.info(
+            "{} disconnected with code {}".format(
+                address_to_str(self.client), code
+            )
+        )
+
+
+if __name__ == "__main__":
+    app.run()
